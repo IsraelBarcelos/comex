da2f14c20399ac8eb4b882ef81915390f944e014
Israel da Rosa Barcelos <israel.barcelos@unilasalle.edu.br>
2022-05-17 14:43:48 -0300

    Main limpa

--- src/main/java/br/com/alura/comex/Main.java
+++ src/main/java/br/com/alura/comex/Main.java
@@ -1,11 +1,9 @@
 package br.com.alura.comex;
 
+import br.com.alura.services.Fechamento;
 import br.com.alura.services.ProcessadorDeCsv;
-import br.com.alura.services.RelatorioSintetico;
 import java.io.IOException;
-import java.math.BigDecimal;
 import java.net.URISyntaxException;
-import java.util.*;
 
 public class Main {
 
@@ -19,50 +17,6 @@ public class Main {
       e.printStackTrace();
     }
 
-    ArrayList<Pedido> pedidos = listas.getPedidos();
-
-    int totalDeProdutosVendidos = 0;
-    int totalDePedidosRealizados = 0;
-    BigDecimal montanteDeVendas = BigDecimal.ZERO;
-    Pedido pedidoMaisBarato = null;
-    Pedido pedidoMaisCaro = null;
-
-    CategoriasProcessadas categoriasProcessadas = new CategoriasProcessadas();
-    int totalDeCategorias = 0;
-
-    pedidoMaisBarato =
-      pedidos.stream().min(Comparator.comparing(Pedido::getPreco)).get();
-    pedidoMaisCaro =
-      pedidos.stream().max(Comparator.comparing(Pedido::getPreco)).get();
-
-    montanteDeVendas =
-      pedidos.stream().map(Pedido::getValorTotal).reduce(BigDecimal::add).get();
-
-    totalDeProdutosVendidos =
-      pedidos.stream().map(Pedido::getQuantidade).reduce(Integer::sum).get();
-
-    totalDePedidosRealizados = pedidos.size();
-
-    pedidos
-      .stream()
-      .forEach(
-        pedido -> {
-          if (!categoriasProcessadas.contains(pedido.getCategoria())) {
-            categoriasProcessadas.add(pedido.getCategoria());
-          }
-        }
-      );
-
-    totalDeCategorias = categoriasProcessadas.getTotalDeCategorias();
-
-    new RelatorioSintetico(
-      totalDePedidosRealizados,
-      totalDeProdutosVendidos,
-      totalDeCategorias,
-      montanteDeVendas,
-      pedidoMaisBarato,
-      pedidoMaisCaro,
-      categoriasProcessadas
-    );
+    new Fechamento(new PedidosDeUmFechamento(listas.getPedidos()));
   }
 }
