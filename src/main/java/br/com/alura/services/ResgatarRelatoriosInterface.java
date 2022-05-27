994f797dd3f4436727937e02c47182f8fda3f256
Israel da Rosa Barcelos <israel.barcelos@unilasalle.edu.br>
2022-05-27 04:19:34 -0300

    Alteração de posição no projeto de arquivos

--- src/main/java/br/com/alura/services/ResgatarRelatoriosInterface.java
+++
@@ -1,8 +0,0 @@
-package br.com.alura.services;
-
-import java.io.IOException;
-import java.net.URISyntaxException;
-
-public interface ResgatarRelatoriosInterface {
-  public void execute() throws IOException, URISyntaxException;
-}
