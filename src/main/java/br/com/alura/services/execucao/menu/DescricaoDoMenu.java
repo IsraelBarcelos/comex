003d9935a89ee4d23d86365dee431d5d7a5a8755
Israel da Rosa Barcelos <israel.barcelos@unilasalle.edu.br>
2022-05-27 13:57:19 -0300

    Menu adicionado

---
+++ src/main/java/br/com/alura/services/execucao/menu/DescricaoDoMenu.java
@@ -0,0 +1,12 @@
+package br.com.alura.services.execucao.menu;
+
+public class DescricaoDoMenu {
+
+  public DescricaoDoMenu() {
+    System.out.println("\nSINTETICO - Relatório Sintético");
+    System.out.println("CLIENTES - Relatório de Clientes");
+    System.out.println("PEDIDOS - Relatório de Pedidos");
+    System.out.println("CATEGORIAS - Relatório de Categorias");
+    System.out.println("Qualquer outra opção fechará o programa");
+  }
+}
