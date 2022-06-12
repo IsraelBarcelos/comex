80bb66d17e55c4593940cd1e6458f48074f72d37
= <=>
2022-06-12 10:34:19 -0300

    start

---
+++ src/main/java/br/com/alura/comexspring/ComexspringApplication.java
@@ -0,0 +1,13 @@
+package br.com.alura.comexspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComexspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComexspringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/alura/comexspring/ComexspringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.comexspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComexspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
