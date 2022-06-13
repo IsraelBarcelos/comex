467106e063eda39c7c50c3a964c411017f130ddd
Israel da Rosa Barcelos <israelbarcelos7@gmail.com>
2022-06-13 16:46:45 -0300

    aplicando padrão java, eu acredito

--- src/main/java/br/com/alura/comex/dto/ProdutoDTO.java
+++ src/main/java/br/com/alura/comex/dto/ProdutoDTO.java
@@ -1,42 +1,41 @@
 package br.com.alura.comex.dto;
 
+import br.com.alura.comex.models.Produto;
 import java.math.BigDecimal;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import br.com.alura.comex.models.Produto;
+public class ProdutoDto {
 
-public class ProdutoDTO {
+  private String nome;
+  private String descricao;
+  private BigDecimal precoUnitariopreco;
+  private Integer quantidadeEstoque;
 
-    private String nome;
-    private String descricao;
-    private BigDecimal precoUnitariopreco;
-    private Integer quantidadeEstoque;
+  public ProdutoDto(Produto produto) {
+    this.nome = produto.getNome();
+    this.descricao = produto.getDescricao();
+    this.precoUnitariopreco = produto.getPrecoUnitario();
+    this.quantidadeEstoque = produto.getQuantidadeEstoque();
+  }
 
-    public ProdutoDTO(Produto produto) {
-        this.nome = produto.getNome();
-        this.descricao = produto.getDescricao();
-        this.precoUnitariopreco = produto.getPrecoUnitario();
-        this.quantidadeEstoque = produto.getQuantidadeEstoque();
-    }
+  public String getNome() {
+    return nome;
+  }
 
-    public String getNome() {
-        return nome;
-    }
+  public String getDescricao() {
+    return descricao;
+  }
 
-    public String getDescricao() {
-        return descricao;
-    }
+  public BigDecimal getPrecoUnitariopreco() {
+    return precoUnitariopreco;
+  }
 
-    public BigDecimal getPrecoUnitariopreco() {
-        return precoUnitariopreco;
-    }
+  public Integer getQuantidadeEstoque() {
+    return quantidadeEstoque;
+  }
 
-    public Integer getQuantidadeEstoque() {
-        return quantidadeEstoque;
-    }
-
-    public static List<ProdutoDTO> converter(List<Produto> produtos) {
-        return produtos.stream().map(ProdutoDTO::new).collect(Collectors.toList());
-    }
+  public static List<ProdutoDto> converter(List<Produto> produtos) {
+    return produtos.stream().map(ProdutoDto::new).collect(Collectors.toList());
+  }
 }
