2b47a9b1cb9f71e29e3482afb96b8fd3bf3de9ba
Israel da Rosa Barcelos <israelbarcelos7@gmail.com>
2022-06-13 15:19:47 -0300

    Controller de produto com repositorio

--- src/main/java/br/com/alura/comex/controllers/ProdutoController.java
+++ src/main/java/br/com/alura/comex/controllers/ProdutoController.java
@@ -1,22 +1,28 @@
 package br.com.alura.comex.controllers;
 
-import br.com.alura.comex.dao.ProdutoDAO;
 import br.com.alura.comex.dto.ProdutoDTO;
-import br.com.alura.comex.utils.JPAUtil;
-
+import br.com.alura.comex.models.Produto;
+import br.com.alura.comex.repository.ProdutoRepository;
 import java.util.List;
-
-import javax.persistence.EntityManager;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ProdutoController {
-    @RequestMapping("/produtos")
-    public List<ProdutoDTO> index() {
-        EntityManager em = JPAUtil.getEntityManager();
-        ProdutoDAO produto = new ProdutoDAO(em);
-        return ProdutoDTO.converter(produto.listaTodos());
-    }
+
+  @Autowired
+  private ProdutoRepository produtoRepository;
+
+  @RequestMapping("/produtos")
+  public List<ProdutoDTO> index() {
+    List<Produto> produtos = produtoRepository.findAll();
+    return ProdutoDTO.converter(produtos);
+  }
+
+  @RequestMapping("/produtos/{id}")
+  public ProdutoDTO show(Long id) {
+    Produto produto = produtoRepository.findById(id).get();
+    return new ProdutoDTO(produto);
+  }
 }
