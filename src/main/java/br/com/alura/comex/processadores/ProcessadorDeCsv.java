1fbfa7c4b1850c0450af9236f24b1a00f4f49a14
Israel da Rosa Barcelos <israelbarcelos7@gmail.com>
2022-06-13 13:58:05 -0300

    pastas DAO do projeto antigo deletadas + alteração no processador de Csv

---
+++ src/main/java/br/com/alura/comex/processadores/ProcessadorDeCsv.java
@@ -0,0 +1,153 @@
+package br.com.alura.comex.processadores;
+
+import br.com.alura.comex.builders.CategoriaBuilder;
+import br.com.alura.comex.builders.ClienteBuilder;
+import br.com.alura.comex.builders.EnderecoBuilder;
+import br.com.alura.comex.builders.ItemPedidoBuilder;
+import br.com.alura.comex.builders.PedidoBuilder;
+import br.com.alura.comex.builders.ProdutoBuilder;
+import br.com.alura.comex.models.Categoria;
+import br.com.alura.comex.models.Cliente;
+import br.com.alura.comex.models.ItemPedido;
+import br.com.alura.comex.models.Pedido;
+import br.com.alura.comex.models.Produto;
+import br.com.alura.comex.models.TipoDescontoPedido;
+import br.com.alura.comex.repository.CategoriaRepository;
+import br.com.alura.comex.repository.ClienteRepository;
+import br.com.alura.comex.repository.ProdutoRepository;
+import br.com.alura.comex.utils.JPAUtil;
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Path;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class ProcessadorDeCsv implements ProcessadorInterface {
+
+  private String path;
+
+  @Autowired
+  private ProdutoRepository produtoRepository;
+
+  @Autowired
+  private CategoriaRepository categoriaRepository;
+
+  @Autowired
+  private ClienteRepository clienteRepository;
+
+  public ProcessadorDeCsv(String path) {
+    this.path = path;
+  }
+
+  public static void main(String[] args) {
+    ProcessadorDeCsv processador = new ProcessadorDeCsv("pedidos.csv");
+    try {
+      processador.execute();
+    } catch (IOException | URISyntaxException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public void execute() throws IOException, URISyntaxException {
+    URL recursoCSV = ClassLoader.getSystemResource(this.path);
+
+    if (recursoCSV == null) {
+      throw new IllegalArgumentException("Arquivo não encontrado");
+    }
+
+    Path caminhoDoArquivo = Path.of(recursoCSV.toURI());
+
+    Scanner leitorDeLinhas = new Scanner(caminhoDoArquivo);
+
+    leitorDeLinhas.nextLine();
+
+    this.transformarDadosEmLinhas(leitorDeLinhas);
+
+    leitorDeLinhas.close();
+  }
+
+  private void transformarDadosEmLinhas(Scanner leitorDeLinhas) {
+    while (leitorDeLinhas.hasNextLine()) {
+      String linha = leitorDeLinhas.nextLine();
+      String[] registro = linha.split(",");
+
+      String categoria = registro[0];
+      Categoria categoriaBanco = new CategoriaBuilder()
+        .comNome(categoria)
+        .ativo()
+        .build();
+
+      categoriaRepository.save(categoriaBanco);
+
+      String produto = registro[1];
+      BigDecimal preco = new BigDecimal(registro[2]);
+      int quantidade = Integer.parseInt(registro[3]);
+
+      Produto produtoBanco = new ProdutoBuilder()
+        .comNome(produto)
+        .comQuantidadeEstoque(Integer.parseInt((registro[3] + 1)))
+        .comPrecoUnitario(preco)
+        .comCategoria(categoriaBanco)
+        .build();
+
+      produtoRepository.save(produtoBanco);
+
+      String nomeDoCliente = registro[5];
+
+      Cliente clienteBanco = clienteRepository.findByNome(nomeDoCliente);
+      if (clienteBanco.getNome() == null) {
+        clienteBanco =
+          new ClienteBuilder()
+            .comNome(nomeDoCliente)
+            .comCpf("cpf")
+            .comTelefone("telefone")
+            .comEndereco(
+              new EnderecoBuilder()
+                .comRua("rua")
+                .comNumero(56)
+                .comBairro("bairro")
+                .comCidade("cidade")
+                .comEstado("estado")
+                .build()
+            )
+            .build();
+
+        clienteRepository.save(clienteBanco);
+      }
+
+      LocalDate data = LocalDate.parse(
+        registro[4],
+        DateTimeFormatter.ofPattern("dd/MM/yyyy")
+      );
+      Pedido pedidoBanco = new PedidoBuilder()
+        .comCliente(clienteBanco)
+        .comData(LocalDate.now())
+        .comDesconto(new BigDecimal(0.00))
+        .comTipoDescontoPedido(TipoDescontoPedido.NENHUM)
+        .comData(data)
+        .build();
+
+      ItemPedido itemPedidoBanco = new ItemPedidoBuilder()
+        .comPedido(pedidoBanco)
+        .comDesconto(pedidoBanco.getDesconto())
+        .comProduto(produtoBanco)
+        .comQuantidade(quantidade)
+        .comPrecoUnitario(produtoBanco.getPrecoUnitario())
+        .build();
+      pedidoBanco.adicionarItemPedido(itemPedidoBanco);
+
+      em.persist(categoriaBanco);
+      em.persist(produtoBanco);
+      em.persist(clienteBanco);
+      em.persist(pedidoBanco);
+      em.persist(itemPedidoBanco);
+
+      em.getTransaction().commit();
+      em.close();
+    }
+  }
+}
