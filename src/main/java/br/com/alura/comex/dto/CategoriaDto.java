de5d951287e369ebc268a7217ba0d303725833ac
Israel da Rosa Barcelos <israelbarcelos7@gmail.com>
2022-06-14 09:37:07 -0300

    Dtos para categoria e cliente

---
+++ src/main/java/br/com/alura/comex/dto/CategoriaDto.java
@@ -0,0 +1,25 @@
+package br.com.alura.comex.dto;
+
+import br.com.alura.comex.models.Categoria;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class CategoriaDto {
+
+  private String nome;
+
+  public CategoriaDto(Categoria categoria) {
+    this.nome = categoria.getNome();
+  }
+
+  public String getNome() {
+    return nome;
+  }
+
+  public static List<CategoriaDto> converter(List<Categoria> categorias) {
+    return categorias
+      .stream()
+      .map(CategoriaDto::new)
+      .collect(Collectors.toList());
+  }
+}
---
+++ src/main/java/br/com/alura/comex/dto/ClienteDto.java
@@ -0,0 +1,99 @@
+package br.com.alura.comex.dto;
+
+import br.com.alura.comex.models.Cliente;
+
+public class ClienteDto {
+    private String nome;
+    private String cpf;
+    private String telefone;
+    private String rua;
+    private Integer numero;
+    private String complemento;
+    private String bairro;
+    private String cidade;
+    private String estado;
+
+    public ClienteDto(Cliente cliente) {
+        this.nome = cliente.getNome();
+        this.cpf = cliente.getCpf();
+        this.telefone = cliente.getTelefone();
+        this.rua = cliente.getEndereco().getRua();
+        this.numero = cliente.getEndereco().getNumero();
+        this.complemento = cliente.getEndereco().getComplemento();
+        this.bairro = cliente.getEndereco().getBairro();
+        this.cidade = cliente.getEndereco().getCidade();
+        this.estado = cliente.getEndereco().getEstado();
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public Integer getNumero() {
+        return numero;
+    }
+
+    public void setNumero(Integer numero) {
+        this.numero = numero;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+}
