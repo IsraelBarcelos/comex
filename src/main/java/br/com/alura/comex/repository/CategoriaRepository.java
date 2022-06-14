e70dc9ab7bb258f7f24874250fd9cd4683ef7c7a
Israel da Rosa Barcelos <israelbarcelos7@gmail.com>
2022-06-14 09:38:16 -0300

    Metodo findByNome de categoria estava com assinatura errada

--- src/main/java/br/com/alura/comex/repository/CategoriaRepository.java
+++ src/main/java/br/com/alura/comex/repository/CategoriaRepository.java
@@ -4,5 +4,5 @@ import br.com.alura.comex.models.Categoria;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
-  Categoria findByNome(Categoria nomeCategoria);
+  Categoria findByNome(String nomeCategoria);
 }
