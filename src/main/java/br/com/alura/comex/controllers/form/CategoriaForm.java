c01714d947e2d06b37174a77ae11e814b914d1e5
Israel da Rosa Barcelos <israelbarcelos7@gmail.com>
2022-06-15 12:04:19 -0300

    add Categoria com length min 2

--- src/main/java/br/com/alura/comex/controllers/form/CategoriaForm.java
+++ src/main/java/br/com/alura/comex/controllers/form/CategoriaForm.java
@@ -3,12 +3,14 @@ package br.com.alura.comex.controllers.form;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
+import org.hibernate.validator.constraints.Length;
 import br.com.alura.comex.builders.CategoriaBuilder;
 import br.com.alura.comex.models.Categoria;
 
 public class CategoriaForm {
   @NotNull
   @NotEmpty
+  @Length(min = 2)
   private String nome;
 
   public String getNome() {
