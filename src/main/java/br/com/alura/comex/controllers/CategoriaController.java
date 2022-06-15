95f0f074acf2815e166a1ee7ccddd84049ff76d7
Israel da Rosa Barcelos <israelbarcelos7@gmail.com>
2022-06-15 18:14:22 -0300

    Categorias com listagem de pedidos por categoria

--- src/main/java/br/com/alura/comex/controllers/CategoriaController.java
+++ src/main/java/br/com/alura/comex/controllers/CategoriaController.java
@@ -2,9 +2,17 @@ package br.com.alura.comex.controllers;
 
 import br.com.alura.comex.controllers.form.CategoriaForm;
 import br.com.alura.comex.dto.CategoriaDto;
+import br.com.alura.comex.dto.PedidoPorCategoriaDto;
 import br.com.alura.comex.models.Categoria;
+import br.com.alura.comex.models.Pedido;
 import br.com.alura.comex.repository.CategoriaRepository;
+import br.com.alura.comex.repository.PedidoRepository;
+
 import java.net.URI;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,9 +29,13 @@ public class CategoriaController {
   @Autowired
   private CategoriaRepository categoriaRepository;
 
+  @Autowired
+  private PedidoRepository pedidoRepository;
+
   @GetMapping
-  public Iterable<Categoria> listar() {
-    return categoriaRepository.findAll();
+  public ResponseEntity<List<CategoriaDto>> index() {
+    List<Categoria> categorias = categoriaRepository.findAll();
+    return ResponseEntity.ok(CategoriaDto.converter(categorias));
   }
 
   @PostMapping
@@ -40,4 +52,17 @@ public class CategoriaController {
 
     return ResponseEntity.created(uri).body(new CategoriaDto(categoria));
   }
+
+  @GetMapping("/pedidos")
+  public ResponseEntity<Map<String, PedidoPorCategoriaDto>> listarPedidosDeUmaCategoria() {
+    List<Categoria> categorias = categoriaRepository.findAll();
+
+    Map<String, List<Pedido>> pedidos = new HashMap<>();
+    categorias.stream().forEach(categoria -> {
+      pedidos.put(categoria.getNome(), pedidoRepository.findByItemPedidoProdutoCategoriaNome(categoria.getNome()));
+    });
+    // categoriaRepository.findPedidosDeUmaCategoria();
+    return ResponseEntity.ok(PedidoPorCategoriaDto.converter(pedidos));
+  }
+
 }
--- src/main/java/br/com/alura/comex/repository/PedidoRepository.java
+++ src/main/java/br/com/alura/comex/repository/PedidoRepository.java
@@ -1,6 +1,14 @@
 package br.com.alura.comex.repository;
 
 import br.com.alura.comex.models.Pedido;
-import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PedidoRepository extends JpaRepository<Pedido, Long> {}
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface PedidoRepository extends JpaRepository<Pedido, Long> {
+
+    @Query("select p from Pedido p join p.itens itens where itens.produto.categoria.nome = ?1")
+    List<Pedido> findByItemPedidoProdutoCategoriaNome(String nomeCategoria);
+}
