54a687b03520acf8df73671b764d4833c0c92eb9
Israel da Rosa Barcelos <israelbarcelos7@gmail.com>
2022-06-27 22:52:51 -0300

    Main recebendo metodo configure

--- src/main/java/br/com/alura/comex/ComexApplication.java
+++ src/main/java/br/com/alura/comex/ComexApplication.java
@@ -2,6 +2,8 @@ package br.com.alura.comex;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
@@ -11,10 +13,15 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSpringDataWebSupport
 @EnableCaching
 @EnableSwagger2
-public class ComexApplication {
+public class ComexApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ComexApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ComexApplication.class);
 	}
 
 }
