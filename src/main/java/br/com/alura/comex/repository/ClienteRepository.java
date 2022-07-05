059c984dac6a6367d98f6466187f5efe7fe7eaeb
Israel da Rosa Barcelos <israelbarcelos7@gmail.com>
2022-07-05 03:02:40 -0300

    mudando o resgate de cliente para optional

--- src/main/java/br/com/alura/comex/repository/ClienteRepository.java
+++ src/main/java/br/com/alura/comex/repository/ClienteRepository.java
@@ -2,8 +2,10 @@ package br.com.alura.comex.repository;
 
 import br.com.alura.comex.models.Cliente;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
-  Cliente findByNome(String nomeDoCliente);
+  Optional<Cliente> findByNome(String nomeDoCliente);
 }
