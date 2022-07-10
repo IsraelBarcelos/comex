418b6d744166c049f4c2e7245ef1e5bce0174e65
Israel da Rosa Barcelos <israelbarcelos7@gmail.com>
2022-07-10 05:45:57 -0300

    salvando deploy sh e makefile atualizado

--- deploy.sh
+++ deploy.sh
@@ -1,26 +1,17 @@
-package:
-	@ mvn clean package
-
-docker-image-build: package
-	@ docker build -t alura/comex .
-
-run: docker-image-build
-	@ docker-compose up -d
-
-stop:
-	@ docker-compose down -v
+mvn clean package
 
 
-deploy: docker-image-build
-	@ docker login --username=_ --password=$(heroku auth:token) registry.heroku.com
-	@ docker image tag alura/comex registry.heroku.com/comex-alura-docker/web:1
-	@ docker push registry.heroku.com/comex-alura-docker/web:1
-	@ make deploy_on_heroku IMAGE_ID=$$(docker image inspect registry.heroku.com/comex-alura-docker/web:1 -f "{{.Id}}")
+docker build -t alura/comex .
+docker-compose up -d
 
-test:
-# monstra a variavel @ echo $(shell docker ps)
 
-	@ make deploy_on_heroku IMAGE=$$(docker image inspect registry.heroku.com/comex-alura-docker/web:1 -f "{{.Id}}")
+$authToken = heroku auth:token
+
+
+docker login --username=_ --password=$authToken registry.heroku.com
+docker image tag alura/comex registry.heroku.com/comex-alura-docker/web:1
+docker push registry.heroku.com/comex-alura-docker/web:1
+$imageId = docker image inspect registry.heroku.com/comex-alura-docker/web:1 -f "{{.Id}}"
 
 # deploy_on_heroku:
 # 	@ curl -X PATCH \
@@ -30,8 +21,9 @@ test:
 # 					-d '{ "updates": [{ "type": "web", "docker_image": "$$(IMAGE_ID)" }] }' \
 # 					https://api.heroku.com/apps/comex-alura-docker/formation
 
-deploy_on_heroku:
-	@ docker login --username=_ --password=$${heroku auth:token} registry.heroku.com
+
+
+curl -X PATCH -H "Content-Type: application/json" -H "Accept: application/vnd.heroku+json; version=3.docker-releases" -H "Authorization: Bearer $authToken" -d "{\"updates\": {\"type\": \"web\", \"docker_image\": $imageId}}" https://api.heroku.com/apps/comex-alura-docker/formation
 #	@ curl -X PATCH -H "Content-Type: application/json" -H "Accept: application/vnd.heroku+json; version=3.docker-releases" -H "Authorization: Bearer $$(heroku auth:token)" -d "{\"updates\": {\"type\": \"web\", \"docker_image\": ${IMAGE_ID}}}" https://api.heroku.com/apps/comex-alura-docker/formation
 # URL FUNCIONAL	curl -X PATCH -H "Content-Type: application/json" -H "Accept: application/vnd.heroku+json; version=3.docker-releases" -H "Authorization: Bearer f6c88c87-545f-4cce-8e49-0d40a4aed8ca" -d "{\"updates\": [{\"type\": \"web\", \"docker_image\":\"sha256:5773e606fff7f665dd17cabc19e5aae03902993b0a0f6c98f3c8275cdfdb39fe\"}]}" https://api.heroku.com/apps/comex-alura-docker/formation
 	
