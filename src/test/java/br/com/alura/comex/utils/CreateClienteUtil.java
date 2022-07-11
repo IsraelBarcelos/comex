d0921e3c21df7d0b25e9a82b13d96050bf333975
Israel da Rosa Barcelos <israelbarcelos7@gmail.com>
2022-07-10 21:39:19 -0300

    test cliente atualizado + processador comentado

--- src/test/java/br/com/alura/comex/utils/CreateClienteUtil.java
+++ src/test/java/br/com/alura/comex/utils/CreateClienteUtil.java
@@ -5,6 +5,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import br.com.alura.comex.builders.ClienteBuilder;
 import br.com.alura.comex.builders.EnderecoBuilder;
 import br.com.alura.comex.models.Cliente;
+import br.com.alura.comex.models.Telefone;
 import br.com.alura.comex.repository.ClienteRepository;
 import br.com.alura.comex.repository.PerfilRepository;
 import br.com.alura.comex.repository.UsuarioRepository;
@@ -20,6 +21,8 @@ public class CreateClienteUtil {
     public final static String bairro = "bairro";
     public final static String cidade = "cidade";
     public final static String estado = "estado";
+    public final static Integer ddd = 54;
+    public final static Integer numeroTelefone = 123456789;
 
     public static void createCliente(ClienteRepository clienteRepository, UsuarioRepository usuarioRepository,
             PasswordEncoder passwordEncoder, PerfilRepository perfilRepository)
@@ -33,7 +36,7 @@ public class CreateClienteUtil {
         Cliente cliente = new ClienteBuilder()
                 .comNome(nome)
                 .comCpf(cpf)
-                .comTelefone(telefone)
+                .comTelefone(new Telefone(ddd, numeroTelefone))
                 .comUsuario(usuarioRepository.findByEmail(CreateUserUtil.email).get())
                 .comEndereco(new EnderecoBuilder()
                         .comBairro(bairro)
